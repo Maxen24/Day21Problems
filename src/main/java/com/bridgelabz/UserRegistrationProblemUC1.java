7c3c5a44ee76fa15cb0935dede047511ab2cd776
Maxen24 <mshubham242@gmail.com>
2022-10-07 00:33:58 +0530

    Added Exception Problem of User Registration Problem UC4

--- src/main/java/com/bridgelabz/UserRegistrationProblemUC1.java
+++ src/main/java/com/bridgelabz/UserRegistrationProblemUC1.java
@@ -49,31 +49,52 @@ public class UserRegistrationProblemUC1 {
         return emailID.matches(emailRegex);
     }
 
-        public static void main (String[]args) {
-            UserRegistrationProblemUC1 myFirstname = new UserRegistrationProblemUC1();
+    public boolean phoneNumber(String phoneNumber) throws CustomExceptions {
+        String PhoneNumberRegex = "^(\\d{2}[- ]?){3}\\d{6}$";
+        phoneNumber.matches(PhoneNumberRegex);
 
-            try {
-                System.out.println(myFirstname.firstName("To"));
-            } catch (Exception e) {
-                System.out.println(e);
-                System.out.println(myFirstname);
-            }
+        if (phoneNumber.matches(PhoneNumberRegex)) {
+            System.out.println("Valid Phone Number" + phoneNumber);
+        } else {
+            throw new CustomExceptions("Invalid Phone Number");
+        }
 
-            UserRegistrationProblemUC2 myLastname=new UserRegistrationProblemUC2();
 
-            try {
-                System.out.println(myLastname.lastName("Be"));
-            } catch (Exception e) {
-                System.out.println(e);
-                System.out.println(myLastname);
-            }
-            UserRegistrationProblemUC3 myEmailID=new UserRegistrationProblemUC3();
+        return (phoneNumber.matches(PhoneNumberRegex));
+    }
 
-            try {
-                System.out.println(myEmailID.emailID("Max.123@gmail.com"));
-            } catch (Exception e) {
-                System.out.println(e);
-                System.out.println(myEmailID);
-            }
+    public static void main(String[] args) {
+        UserRegistrationProblemUC1 myFirstname = new UserRegistrationProblemUC1();
+
+        try {
+            System.out.println(myFirstname.firstName("To"));
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(myFirstname);
+        }
+
+        UserRegistrationProblemUC2 myLastname = new UserRegistrationProblemUC2();
+
+        try {
+            System.out.println(myLastname.lastName("Be"));
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(myLastname);
+        }
+        UserRegistrationProblemUC3 myEmailID = new UserRegistrationProblemUC3();
+
+        try {
+            System.out.println(myEmailID.emailID("Max.123@gmail.com"));
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(myEmailID);
+        }
+        UserRegistrationProblemUC1 myPhoneNumber = new UserRegistrationProblemUC1();
+        try {
+            System.out.println(myPhoneNumber.phoneNumber("91@9695795786"));
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println(myPhoneNumber);
         }
     }
+}
